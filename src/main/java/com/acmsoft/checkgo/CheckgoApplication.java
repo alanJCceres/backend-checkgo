3ed0465046eeac69000f7397f535b38ea882a62f
Alan Cáceres <caceresalan2474@gmail.com>
2026-08-11 21:51:28 -0400

    Initial commit

---
+++ src/main/java/com/acmsoft/checkgo/CheckgoApplication.java
@@ -0,0 +1,13 @@
+package com.acmsoft.checkgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acmsoft/checkgo/CheckgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmsoft.checkgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
